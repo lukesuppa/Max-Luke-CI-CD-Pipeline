15ba029ae342e1efeba59c0a829e590a05f4386e
Maxwell Houck <99267984+maxwellhouck@users.noreply.github.com>
2022-04-25 10:23:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,14 @@
+
 from flask import Flask
+
+# This line creates the Flask app as a global variable
 app = Flask(__name__)
 
-def read_count():
-    try:
-        with open('count.txt', 'r') as f:
-            count = int(f.read())
-        return count
-    except IOError:
-        return 0
-
-
-def save_count(count):
-    with open('count.txt', 'w') as f:
-        f.write(str(count))
-
-
-@app.route("/")
+# This annotation tells Flask to route any traffic for / to this function
+@app.route('/')
 def hello():
-    count = read_count()    
-    count += 1
-    save_count(count);
-    return "<h1 style='color:red'>Hello World! {}</h1>".format(count)
+    return '<h1>Hello World</h1>'
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+if __name__ == '__main__':
+    app.run(port=8000)
