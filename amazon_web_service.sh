0ea6baec1f0493eb3767e89de85d2c36462b352e
Maxwell Houck <99267984+maxwellhouck@users.noreply.github.com>
2022-04-25 10:16:50 -0400

    Update amazon_web_service.sh

--- amazon_web_service.sh
+++ amazon_web_service.sh
@@ -5,4 +5,4 @@ git clone https://github.com/cs220s22/Max-Luke_CI-CD_Pipeline.git
 cd Max-Luke-CI-CD-Pipeline
 python3 -m venv .venv
 .venv/bin/pip install -r requirements.txt
-sudo .venv/bin/python3 -b :80 simple_app.py &
+sudo .venv/bin/python3 -b :80 app.py &
