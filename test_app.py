f7e1f4e977bc786e62f09247ef868c60f1e565c0
lukesuppa <99268105+lukesuppa@users.noreply.github.com>
2022-04-28 14:30:55 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -6,11 +6,6 @@ def test_hello():
 
     """put the app into testing mode"""
     app.config.update({"TESTING": True,})
-    """get the test client"""
     client = app.test_client()
-
-    """use the test client to call the / endpoint, save the response"""
     response = client.get('/')
-
-    """verify that the data of the response is what we expect"""
     assert b"<h1>Hello World</h1>" in response.data
