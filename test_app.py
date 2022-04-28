1e44074d61f1b4e75915ba3004935c0210529e9d
lukesuppa <99268105+lukesuppa@users.noreply.github.com>
2022-04-28 14:27:14 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,16 +1,16 @@
 
-# Import the app variable from app.py
+"""Import the app variable from app.py"""
 from app import app
 
 def test_hello():
 
-    # put the app into testing mode
+    """put the app into testing mode"""
     app.config.update({"TESTING": True,})
-    # get the test client
+    """get the test client"""
     client = app.test_client()
 
-    # use the test client to call the / endpoint, save the response
+    """use the test client to call the / endpoint, save the response"""
     response = client.get('/')
 
-    # verify that the data of the response is what we expect
+    """verify that the data of the response is what we expect"""
     assert b"<h1>Hello World</h1>" in response.data
